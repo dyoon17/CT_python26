1358514051c579d22852f6a2e7a498bb13f93c92
Doyoon <nkdy08@gmail.com>
2026-09-14 20:37:27 +0900

    [level 0] Title: k의 개수, Time: 19.39 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -0,0 +1,10 @@
+def solution(i, j, k):
+    answer = 0
+    
+    for num in range(i, j+1):
+        if str(k) in str(num):
+            answer += str(num).count(str(k))
+        else:
+            continue
+            
+    return answer
