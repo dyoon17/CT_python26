0ad52f2fb909062283f655afb13f82113c11b9bb
Doyoon <nkdy08@gmail.com>
2026-09-15 17:38:33 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,9 @@
+def solution(common):
+    answer = 0
+        
+    if (common[0] + common[2]) / 2 == common[1]:    # 등차
+        answer = common[-1] + (common[1] - common[0])
+    else:   # 등비
+        answer = common[-1] * (common[1] / common[0])
+    
+    return answer
